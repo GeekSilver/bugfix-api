854bfdff1cac4282bc507bdd54e42af67a01899f
GeekSilver <erickioko995@gmail.com>
2020-11-16 18:00:57 +0300

    deploy: replaced hard coded database settings with env options for deploying.

--- config/database.js
+++ config/database.js
@@ -1,18 +1,19 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'bugfix'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', 'password'),
-        ssl: env.bool('DATABASE_SSL', false),
+  "defaultConnection": "default",
+  "connections": {
+    "default": {
+      "connector": "bookshelf",
+      "settings": {
+        "client": "postgres",
+        "host": "${process.env.DATABASE_HOST}",
+        "port": "${process.env.DATABASE_PORT}",
+        "database": "${process.env.DATABASE_NAME}",
+        "username": "${process.env.DATABASE_USERNAME}",
+        "password": "${process.env.DATABASE_PASSWORD}",
+        "ssl": { "rejectUnauthorized": false }
       },
-      options: {}
-    },
-  },
-});
+      "options": {}
+    }
+  }
+}
+);
