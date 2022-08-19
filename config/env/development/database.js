6b1f72346c205b2d831eabec2007049bb23c5f29
gs <erickioko995@gmail.com>
2022-08-19 15:14:14 +0300

    update(migration): migrated from strapi 3.3.2 to strapi 3.6.9

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => ({
         host: env("DATABASE_HOST", "127.0.0.1"),
         port: env.int("DATABASE_PORT", 5432),
         database: env("DATABASE_NAME", "bugfix"),
-        username: env("DATABASE_USERNAME", "root"),
+        username: env("DATABASE_USERNAME", "gs"),
         password: env("DATABASE_PASSWORD", "password"),
         ssl: env.bool("DATABASE_SSL", false),
       },
