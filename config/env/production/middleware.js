23c028e7d210dbd7b381ad8c5751b50b63f99ad8
gs <erickioko995@gmail.com>
2022-08-19 15:40:30 +0300

    patch: disabled cors on production

--- config/env/production/middleware.js
+++ config/env/production/middleware.js
@@ -1,7 +1,7 @@
 module.exports = {
   settings: {
     cors: {
-      enabled: true,
+      enabled: false,
       origin: [
         "https://bugfix-dev.herokuapp.com",
         "http://bugfix-dev.herokuapp.com",
